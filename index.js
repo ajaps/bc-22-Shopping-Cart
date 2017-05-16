05d6a6d0bd3a82871afac28fcdc74181248d34f8
AjapsFranklin <framky007@yahoo.com>
2017-05-16 08:00:38 +0100

    improves server file to work on Heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,11 +10,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-/*
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(__dirname + '/index');
 });
-*/
+
+app.get('/login', function (request, response){
+	response.sendFile(path.join(__dirname,'public','login.html'));
+});
+
+app.get('/register', function (request, response){
+	response.sendFile(path.join(__dirname,'public','register.html'));
+});
+
+app.get('/userPage', function (request, response){
+	response.sendFile(path.join(__dirname,'public','userPage.html'));
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
